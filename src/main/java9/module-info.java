254dbdc82c6595fea160506095d362b7c8a7073c
Stefano Cordio <stefano_cordio@epam.com>
2024-07-30 01:06:28 +0200

    Add module descriptor

---
+++ src/main/java9/module-info.java
@@ -0,0 +1,27 @@
+/*
+ * Copyright © 2024 Stefano Cordio (stefano.cordio@gmail.com)
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+/**
+ * JUnit Jupiter {@link org.junit.jupiter.api.io.TempDir} extension based on {@link
+ * com.google.common.jimfs.Jimfs}.
+ */
+@SuppressWarnings("requires-transitive-automatic")
+module jimfs.junit.jupiter {
+    requires transitive com.google.common.jimfs;
+    requires org.junit.jupiter.api;
+
+    exports io.github.scordio.jimfs.junit.jupiter;
+}
