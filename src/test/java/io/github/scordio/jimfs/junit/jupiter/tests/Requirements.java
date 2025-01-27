3043da3b8016b59803825d3279e14f64da400365
Stefano Cordio <stefano.cordio@gmail.com>
2025-01-27 01:10:49 +0100

    Fix module name, align build to `maven-compile-plugin` guidelines

--- src/test/java/io/github/scordio/jimfs/junit/jupiter/tests/Requirements.java
+++ src/test/java/io/github/scordio/jimfs/junit/jupiter/tests/Requirements.java
@@ -13,7 +13,7 @@
  * See the License for the specific language governing permissions and
  * limitations under the License.
  */
-package io.github.scordio.jimfs.junit.jupiter;
+package io.github.scordio.jimfs.junit.jupiter.tests;
 
 import static org.assertj.core.api.Assertions.assertThat;
 
