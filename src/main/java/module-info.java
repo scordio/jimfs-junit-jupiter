0134abe719dca8c26364053df02a3239f64fe780
Stefano Cordio <stefano.cordio@gmail.com>
2026-03-28 21:37:30 +0100

    Update copyright year

--- src/main/java/module-info.java
+++ src/main/java/module-info.java
@@ -1,5 +1,5 @@
 /*
- * Copyright © 2024-2025 Stefano Cordio
+ * Copyright © 2024-present Stefano Cordio
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
@@ -13,7 +13,6 @@
  * See the License for the specific language governing permissions and
  * limitations under the License.
  */
-
 /**
  * JUnit Jupiter {@link org.junit.jupiter.api.io.TempDir} extension based on
  * {@link com.google.common.jimfs.Jimfs}.
